9dd99160abb89cde2ab3a91061ceeb4d69b042f7
surajkaushik1 <24B11CS493@adityauniversity.in>
2026-08-10 15:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.02%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -3,7 +3,12 @@ public:
     string toLowerCase(string s) {
         int n = s.size();
         for(int i=0;i<n;i++){
-            s[i] = tolower(s[i]);
+            if((s[i]) <= 90 && s[i] >= 65 ){
+                s[i] = (s[i] + 32);
+            }
+            else{
+                s[i] = s[i];
+            }
         }
         return s;
     }
