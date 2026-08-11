d105e97c9deded396ddf8927f005142e752130ff
surajkaushik1 <24B11CS493@adityauniversity.in>
2026-08-11 14:31:20 +0530

    Time: 3 ms (43.20%) | Memory: 8.2 MB (76.37%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string interpret(string c) {
+        int n = c.size();
+        string s = "";
+        for(int i=0;i<n;i++){
+            if(c[i]=='(' && c[i+1]==')'){
+                s += 'o';
+            }
+            else if(c[i]=='(' && c[i+1]=='a' && c[i+2]=='l'&& c[i+3]==')'){
+                s += 'a';
+                s += 'l';
+            }
+            else if(c[i] == 'G'){
+                s += 'G';
+            }
+        }
+        return s;
+    }
+};
