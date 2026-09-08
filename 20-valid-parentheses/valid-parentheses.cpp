40ba67911dd946380033fdf23c3b31e73f246c36
surajkaushik1 <24B11CS493@adityauniversity.in>
2026-09-08 15:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (6.22%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='{' || s[i]=='[' || s[i]=='('){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(s[i]==')' && st.top()!='('){
+                    return 0;
+                }
+                else if(s[i]==']' && st.top()!='['){
+                    return 0;
+                }
+                else if(s[i]=='}' && st.top()!='{'){
+                    return 0;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
